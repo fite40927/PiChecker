1df3ca090b3f7673f3d67ff09abf89e6fa0c67f4
Apsara <38597307+fite40927@users.noreply.github.com>
2019-10-04 13:15:07 -0700

    Pi Checker

---
+++ src/Resources.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Resources {
+	public static Scanner scan = new Scanner(System.in);
+	public static String print(String s) {
+		System.out.print(s);
+		return s;
+	}
+	public static String print(int s) {
+		System.out.print(s);
+		return Integer.toString(s);
+	}
+	public static String print(double s) {
+		System.out.print(s);
+		return Double.toString(s);
+	}
+	public static Object print(Object s) {
+		System.out.print(s);
+		return s;
+	}
+	public static char print(char s) {
+		System.out.print(s);
+		return s;
+	}
+	
+	public static String println(String s) {
+		System.out.println(s);
+		return s;
+	}
+	public static String println(int s) {
+		System.out.println(s);
+		return Integer.toString(s);
+	}
+	public static String println(double s) {
+		System.out.println(s);
+		return Double.toString(s);
+	}
+	public static Object println(Object s) {
+		System.out.println(s);
+		return s;
+	}
+	public static char println(char s) {
+		System.out.println(s);
+		return s;
+	}
+}
